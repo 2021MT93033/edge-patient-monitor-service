5d34f2fcfe99ce85f0e95243d97187422f2150b3
2021MT93033 <balavignesh.premkumar@gmail.com>
2022-11-17 17:31:58 +0530

    Controller Changes

--- edge-patient-monitoring-service/src/main/java/com/bits/assignment/universalt10/edge/patient/edgepatientmonitoringservice/controller/PatientMonitoringController.java
+++ edge-patient-monitoring-service/src/main/java/com/bits/assignment/universalt10/edge/patient/edgepatientmonitoringservice/controller/PatientMonitoringController.java
@@ -3,16 +3,18 @@
  */
 package com.bits.assignment.universalt10.edge.patient.edgepatientmonitoringservice.controller;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.bits.assignment.universalt10.edge.patient.edgepatientmonitoringservice.model.PatientData;
 import com.bits.assignment.universalt10.edge.patient.edgepatientmonitoringservice.model.SimpleMessage;
 import com.bits.assignment.universalt10.edge.patient.edgepatientmonitoringservice.service.PatientMonitoringService;
-
-import io.swagger.v3.oas.annotations.parameters.RequestBody;
 
 /**
  * @author Vicky
@@ -24,9 +26,17 @@ public class PatientMonitoringController {
 	@Autowired
 	private PatientMonitoringService service;
 
+	private Logger log = LoggerFactory.getLogger(this.getClass());
+
+	@GetMapping("/health")
+	public ResponseEntity<SimpleMessage> healthCheck() {
+		return ResponseEntity.ok(new SimpleMessage("The Service is Running successfully"));
+	}
+
 	@PostMapping("/monitor")
 	public ResponseEntity<SimpleMessage> monitorPatientData(@RequestBody PatientData patientData) {
 		SimpleMessage message = service.monitorPatientData(patientData);
+		message.setMessage("Sent Successfully");
 		return ResponseEntity.ok(message);
 	}
 
--- edge-patient-monitoring-service/src/main/java/com/bits/assignment/universalt10/edge/patient/edgepatientmonitoringservice/service/impl/PatientMonitoringServiceImpl.java
+++ edge-patient-monitoring-service/src/main/java/com/bits/assignment/universalt10/edge/patient/edgepatientmonitoringservice/service/impl/PatientMonitoringServiceImpl.java
@@ -6,6 +6,8 @@ package com.bits.assignment.universalt10.edge.patient.edgepatientmonitoringservi
 import java.util.Collections;
 
 import org.apache.commons.lang3.StringUtils;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.HttpEntity;
@@ -31,6 +33,8 @@ public class PatientMonitoringServiceImpl implements PatientMonitoringService {
 
 	@Autowired
 	private RestTemplate restTemplate;
+
+	private Logger log = LoggerFactory.getLogger(this.getClass());
 
 	@Override
 	public SimpleMessage monitorPatientData(PatientData patientData) {
